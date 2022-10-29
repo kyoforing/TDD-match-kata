abe134c1ec1f0f64a72cc16d1c1514f10ffeb8df
Kyo Cheng <Kyo Cheng>
2022-10-29 23:31:26 +0800

    refactor: resolve feature envy

--- MatchKata/Models/Match.cs
+++ MatchKata/Models/Match.cs
@@ -1,9 +1,51 @@
-﻿namespace MatchKata.Models
+﻿using MatchKata.Enums;
+using MatchKata.Services;
+
+namespace MatchKata.Models
 {
     public class Match
     {
+        private const char SecondHalfSymbol = ';';
+        private const string HomeGoalSymbol = "H";
+        private const string AwayGoalSymbol = "A";
         public int Id { get; set; }
         public int LivePeriod { get; set; }
         public string GoalRecord { get; set; }
+
+        public void ProcessGoalRecord(MatchEvent matchEvent)
+        {
+            if (IsNeedToAddHalfSymbol())
+            {
+                GoalRecord += ";";
+            }
+
+            if (matchEvent.EnumMatchEvent == EnumMatchEvent.HomeGoal)
+            {
+                AddRecord(HomeGoalSymbol);
+            }
+            else if (matchEvent.EnumMatchEvent == EnumMatchEvent.AwayGoal)
+            {
+                AddRecord(AwayGoalSymbol);
+            }
+            else if ((matchEvent.EnumMatchEvent == EnumMatchEvent.CancelHomeGoal && GoalRecord.EndsWith(HomeGoalSymbol)) || (matchEvent.EnumMatchEvent == EnumMatchEvent.CancelAwayGoal && GoalRecord.EndsWith(AwayGoalSymbol)))
+            {
+                CancelRecord();
+            }
+        }
+
+        private string CancelRecord()
+        {
+            return GoalRecord = GoalRecord.Substring(0, GoalRecord.Length - 1);
+        }
+
+        private string AddRecord(string record)
+        {
+            return GoalRecord += record;
+        }
+
+        private bool IsNeedToAddHalfSymbol()
+        {
+            return LivePeriod == 2 && !GoalRecord.Contains(SecondHalfSymbol);
+        }
     }
 }
--- MatchKata/Services/MatchService.cs
+++ MatchKata/Services/MatchService.cs
@@ -1,4 +1,5 @@
 ﻿using MatchKata.Enums;
+using MatchKata.Models;
 using MatchKata.Repositories;
 
 namespace MatchKata.Services
@@ -15,29 +16,7 @@ namespace MatchKata.Services
         public void AddEvent(MatchEvent matchEvent)
         {
             var match = _matchRepository.GetMatch(matchEvent.Id);
-
-            if (match.LivePeriod == 2 && !match.GoalRecord.Contains(';'))
-            {
-                match.GoalRecord += ";";
-            }
-
-            if (matchEvent.EnumMatchEvent == EnumMatchEvent.HomeGoal)
-            {
-                match.GoalRecord += "H";
-            }
-            else if (matchEvent.EnumMatchEvent == EnumMatchEvent.CancelHomeGoal && match.GoalRecord.EndsWith("H"))
-            {
-                match.GoalRecord = match.GoalRecord.Substring(0, match.GoalRecord.Length - 1);
-            }
-            else if (matchEvent.EnumMatchEvent == EnumMatchEvent.CancelAwayGoal && match.GoalRecord.EndsWith("A"))
-            {
-                match.GoalRecord = match.GoalRecord.Substring(0, match.GoalRecord.Length - 1);
-            }
-            else
-            {
-                match.GoalRecord += "A";
-            }
-
+            match.ProcessGoalRecord(matchEvent);
             _matchRepository.UpdateMatch(match);
         }
     }
